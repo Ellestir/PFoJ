da460339adf63f8793889b93b39da997aa5102fb
saluun <osaluun@gmail.com>
2018-10-21 18:02:06 +0200

    Kopf angepasst

--- W1/T4/StuckInATimeLoop.java
+++ W1/T4/StuckInATimeLoop.java
@@ -1,11 +1,11 @@
 /**
 * Advanced Object Oriented Programming with Java, WS 2018
-* Problem: <Add problem here>
-* Link: <Add link here>
+* Problem: Stuck In A Time Loop
+* Link: open.kattis.com/problems/timeloop
 * @author Fabian Grun (1230840)
 * @author Selin Can (1101908)
 * @author Tom Rosenberger (1208597)
-* @version 1.0, <Add date here>
+* @version 1.0, 21.10.2018
 *
 * Method : <Add method name>
 * Status: <Add status>
