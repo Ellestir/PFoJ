9aa3d1bfa60a0253af4bea080d0332e4a301117d
saluun <osaluun@gmail.com>
2018-10-21 18:01:06 +0200

    fertige B mit Kommentar Kopf

--- W1/T2/Coldputer.java
+++ W1/T2/Coldputer.java
@@ -1,3 +1,17 @@
+/**
+* Advanced Object Oriented Programming with Java, WS 2018
+* Problem: Coldputer
+* Link: open.kattis.com/problems/cold
+* @author Fabian Grun (1230840)
+* @author Selin Can (1101908)
+* @author Tom Rosenberger (1208597)
+* @version 1.0, 21.10.2018
+*
+* Method : <Add method name>
+* Status: <Add status>
+* Runtime: <Add runtime>
+*/
+
 package oop;
 
 import java.util.Scanner;
